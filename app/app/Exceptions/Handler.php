08040034953a5244e2f3e4f1738d6088ba5f3461
Veljko Stević <stevic.sv@gmail.com>
2021-04-06 00:39:07 +0200

    modify exceptions to support json responser format

--- app/app/Exceptions/Handler.php
+++ app/app/Exceptions/Handler.php
@@ -2,40 +2,49 @@
 
 namespace App\Exceptions;
 
+use App\Traits\JsonResponser;
+use Illuminate\Http\Response;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
+	use JsonResponser;
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
+	/**
+	 * A list of the exception types that are not reported.
+	 *
+	 * @var array
+	 */
+	protected $dontReport = [
+		//
+	];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
-    }
+	/**
+	 * A list of the inputs that are never flashed for validation exceptions.
+	 *
+	 * @var array
+	 */
+	protected $dontFlash = [
+		'current_password',
+		'password',
+		'password_confirmation',
+	];
+
+	/**
+	 * Register the exception handling callbacks for the application.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->renderable(function (ValidationException $ex) {
+			return $this->sendErrorResponse($ex->validator->getMessageBag(), 'The given data is invalid!', Response::HTTP_UNPROCESSABLE_ENTITY);
+		});
+
+		$this->renderable(function (AuthenticationException $ex) {
+			return $this->sendErrorResponse(null, $ex->getMessage(), Response::HTTP_UNAUTHORIZED);
+		});
+	}
 }
